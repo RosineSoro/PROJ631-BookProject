030836a609bb6ee2b4f71ca275949669c972b8e4
coltarm <113551889+coltarm@users.noreply.github.com>
2023-05-05 15:21:14 +0200

    ajout connexion à une base de données

---
+++ Page_web/create_account.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="create_account.css">
+	<title>créer un compte</title>
+</head>
+<body>
+	
+<?php
+<?php
+try
+{
+	$db = new PDO('mysql:host=localhost;dbname=projet_livre;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+       die('erreur : ' . $e->getMessage());
+}
+?>
+<?php
+	
+		if(isset($_POST['username']) and isset($_POST['user_email']) and isset($_POST['create_password']) and isset($_POST['verify_password'])){
+			if ($_POST['create_password'] == $_POST['verify_password']){
+				$hash = password_hash($_POST["password"], PASSWORD_DEFAULT)
+				$sql = "insert into account(username,password) values ";
+				
+			}
+			else{
+				echo('le mots de passe et la verification mots de passe ne correspondent pas');
+			}
+			
+		}
+
+	?>
+
+
+	<div id="connect">
+		<h4>Connexion</h4>
+		<form method="post" , id="form_connect">
+			<input type="text" placeholder="nom d'utilisateur" name="username">
+			<input type="email" place holder="email" name="user_emair">
+			<input type="password" placeholder="mot de passe" name="create_password">
+			<input type="password" placeholder="vérification de mot de passe" name="verify_password">
+
+			<button typr="submit">connexion</button>
+		</form>
+	</div>
+
+</body>
+</html>
--- Page_web/form_connect.php
+++ Page_web/form_connect.php
@@ -7,20 +7,40 @@
 	<title>Page Accueille</title><i class="fas fa-search"></i>
 </head>
 <body>
+
+	<?php
+try
+{
+	$db = new PDO('mysql:host=localhost;dbname=projet_livre;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+       die('erreur : ' . $e->getMessage());
+}
+?>
+
+<?php
+	
+		if(isset($_POST['user']) and isset($_POST['password'])){
+			$hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+			$sql = "select username,password from account where user = '".$_POST['user'] ."' and password ='". $hash ."'";
+			echo("<h1>".$hash."</h1>");
+		}
+
+	?>
+
 	<div id="content">
 	<h1>Titre</h1>
 	<a href="https://openclassrooms.com/fr/"><img src="logo/logo_connexion.jpg" alt="petit bonhome de connection"></a>
 	<div id="connect">
 		<h4>Connexion</h4>
-		<form method="post" , id="form_connect">
+		<form method="post" action="http://localhost/Page_web/form_connect.php" id="form_connect">
 			<input type="text" placeholder="nom d'utilisateur" name="user">
 			<input type="password" placeholder="mot de passe" name="password">
 			<button typr="submit">connexion</button>
 		</form>
 	</div>
-	<?php 
-		echo("bonjour");
-	?>
+
 
 
 	<a id="create_account" href="creer_un_compte.html">Créer un compte</a>
