5220ea3eee8704af22d7c7efce101a4d0a49cc8b
unknown <sarahcadm@protonmail.com>
2023-05-09 12:22:43 +0200

    tables.sql OK.

--- sql/tables.sql
+++ sql/tables.sql
@@ -5,7 +5,7 @@ CREATE TABLE account (
 	username varchar(40) PRIMARY KEY NOT NULL, 
 	description varchar(200), 
 	visibility varchar(20) NOT NULL DEFAULT 'private', 
-	password varchar(200) NOT NULL
+	password varchar(200) NOT NULL,
 	id_author INT,
 	CONSTRAINT fk_id_author FOREIGN KEY (id_author) REFERENCES author(id_author) ON DELETE SET NULL ON UPDATE CASCADE	
 );
@@ -23,8 +23,7 @@ CREATE TABLE author (
 
 DROP TABLE IF EXISTS genre;
 CREATE TABLE genre (
-	id_genre int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT, 
-	name varchar(40), 
+	id_genre varchar(40) PRIMARY KEY NOT NULL, 
 	description varchar(200) 
 );
 
@@ -32,13 +31,13 @@ CREATE TABLE genre (
 
 DROP TABLE IF EXISTS book;
 CREATE TABLE book (
-	id_book int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+	id_book varchar(40) PRIMARY KEY NOT NULL, 
 	title varchar(40), 
-	id_author int(10), 
+	id_author INT, 
 	/* augmentait la taille du lien de l'image parceque si le lien vient de internet ça va être compliqué,
 	j'en ai besoin pour faire des tests*/
 	img varchar(255), 
-	id_genre int(10), 
+	id_genre varchar(40), 
 	plot varchar(200), 
 	sales int(10), 
 	rdate datetime, 
@@ -49,9 +48,13 @@ CREATE TABLE book (
 /** Table review **/
 
 DROP TABLE IF EXISTS review;
-CREATE TABLE review (id_review int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT, 
-	username varchar(40), id_book int(10), rating int(10), 
-	content varchar(200), post_date datetime, 
+CREATE TABLE review (
+	id_review int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+	username varchar(40), 
+	id_book varchar(40), 
+	rating int(10), 
+	content varchar(200), 
+	post_date datetime, 
 	CONSTRAINT lien_rev_acc FOREIGN KEY (username) REFERENCES account(username) ON DELETE RESTRICT ON UPDATE CASCADE, 
 	CONSTRAINT lien_rev_book FOREIGN KEY (id_book) REFERENCES book(id_book) ON DELETE RESTRICT ON UPDATE CASCADE
 );
@@ -60,11 +63,11 @@ CREATE TABLE review (id_review int(10) PRIMARY KEY NOT NULL AUTO_INCREMENT,
 
 DROP TABLE IF EXISTS follow;
 CREATE TABLE follow (
-	id_follower int(10), 
-	id_followed int(10), 
-	PRIMARY KEY (id_follower, id_followed),
-	CONSTRAINT fk_id_follower FOREIGN KEY (id_follower) REFERENCES account(username) ON DELETE RESTRICT ON UPDATE CASCADE,
-	CONSTRAINT fk_id_followed FOREIGN KEY (id_followed) REFERENCES account(username) ON DELETE RESTRICT ON UPDATE CASCADE
+	follower varchar(40), 
+	followed varchar(40), 
+	PRIMARY KEY (follower, followed),
+	CONSTRAINT fk_id_follower FOREIGN KEY (follower) REFERENCES account(username) ON DELETE RESTRICT ON UPDATE CASCADE,
+	CONSTRAINT fk_id_followed FOREIGN KEY (followed) REFERENCES account(username) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 /** Table interest **/
@@ -72,7 +75,7 @@ CREATE TABLE follow (
 DROP TABLE IF EXISTS interest;
 CREATE TABLE interest (
 	username varchar(40), 
-	id_genre int(10), 
+	id_genre varchar(40), 
 	PRIMARY KEY(username, id_genre), 
 	CONSTRAINT lien_interest_acc FOREIGN KEY (username) REFERENCES account(username) ON DELETE RESTRICT ON UPDATE CASCADE, 
 	CONSTRAINT lien_interest_genre FOREIGN KEY (id_genre) REFERENCES genre(id_genre) ON DELETE RESTRICT ON UPDATE CASCADE
@@ -83,8 +86,8 @@ CREATE TABLE interest (
 DROP TABLE IF EXISTS want_read;
 CREATE TABLE want_read (
 	username varchar(40), 
-	id_book int(10), 
+	id_book varchar(40), 
 	PRIMARY KEY(username, id_book), 
-	CONSTRAINT lien_wr_acc FOREIGN KEY (username) REFERENCES account(username) ON DELETE RESTRICT ON UPDATE CASCADE, 
-	CONSTRAINT lien_wr_book FOREIGN KEY (id_book) REFERENCES book(id_book) ON DELETE RESTRICT ON UPDATE CASCADE
+	CONSTRAINT fk_wr__acc_username FOREIGN KEY (username) REFERENCES account(username) ON DELETE RESTRICT ON UPDATE CASCADE, 
+	CONSTRAINT fk_wr_book_id FOREIGN KEY (id_book) REFERENCES book(id_book) ON DELETE RESTRICT ON UPDATE CASCADE
 );
