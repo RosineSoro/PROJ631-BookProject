3792dc924caa58c46e710346487dc36d0fbeb6b5
Rosine Soro <gnidanhanrosine@gmail.com>
2023-05-11 10:25:23 +0200

    page d'accueil terminée (reste certains livres à mettre), form_connect : disposition terminée(reste l'héritage de page

--- Page_web/pageAcceuil.php
+++ Page_web/pageAcceuil.php
@@ -18,82 +18,76 @@
    </head> 
 
    <body>
-   	<div id="content
-	
-	        <img src="logo\MouthfulReadersLogo.png" alt="Logo">
-
-   	<?php
-			try
-			{
-				$db = new PDO('mysql:host=localhost;dbname=projet_livre;charset=utf8', 'root', '');
-			}
-			catch (Exception $e)
-			{
-			       die('erreur : ' . $e->getMessage());
-			}
-		?>
-		<?php
-
-		if( !isset($_GET["page"]) ) { 
-        $page=0;
-      }else{
-        $page=$_GET["page"];
-
-      }
-      echo ("<h1>salut</h1>");
-      ?>
-
-		<h3>MOUTHFUL Readers</h3>
-
-      <nav>
-			<div class="search-container">
-			  <form class="search" action="#" method="GET">
-				<input type="text" class="search-bar" placeholder="Titre,Thèmes,Livres,..." name="search">
-				<button type="submit" class="search-button" name="submitSearch"><i class="fa fa-search"></i></button>
-			  	</form>	
-        </div>
+   	    <div id="content">
+	 
+	        
+			<?php
+					try
+					{
+						$db = new PDO('mysql:host=localhost;dbname=rosis;charset=utf8', 'root', '');
+					}
+					catch (Exception $e)
+					{
+						   die('erreur : ' . $e->getMessage());
+					}
 		
-		<div class="dropdown">
-			  <button class="dropbtn">
-			     <span>Découvrir </span>
-			     <i class="fa fa-caret-down"></i>
-			  </button>
-			  <div class="dropdown-content">
-					<a href="#">Livres</a>
-					<a href="#">Thèmes</a>
-					<a href="#">Tendance</a>
-					<a href="#">Récents</a>
-              </div>
+
+					if( !isset($_GET["page"]) ) { 
+					       $page=0;
+				    }else{
+					       $page=$_GET["page"];
+				    }
+			 ?>
+			 
+			  
+			<div id ="title-container">  
+					<img src="logo\MouthfulReadersLogo.png" alt="Logo">
+					<h3>MOUTHFUL Readers</h3>
+			</div>		
+
+			<nav>
+			
+				<div class="search-container">
+					  <form class="search" action="#" method="GET">
+						<input type="text" class="search-bar" placeholder="Titre,Thèmes,Livres,..." name="search">
+						<button type="submit" class="search-button" name="submitSearch"><i class="fa fa-search"></i></button>
+					  </form>	
+				</div>
+				
+				<div class="dropdown">
+					  <button class="dropbtn">
+						 <span>Découvrir </span>
+						 <i class="fa fa-caret-down"></i>
+					  </button>
+					  <div class="dropdown-content">
+							<a href="#">Livres</a>
+							<a href="#">Thèmes</a>
+							<a href="#">Tendance</a>
+							<a href="#">Récents</a>
+					  </div>
+				</div>
+
+				<div class="user-container">
+					  <button type = "submit" name = "submitConnection">
+					  <i class="fa fa-user icon"></i>
+					  Se connecter
+					  </button>
+				 </div>
+		    </nav>
+		    <div id="caroussel">
+				<?php 
+				$sql ="select * from book Limit 10";
+				$sqlexec = $db->prepare($sql);
+				$sqlexec ->execute();
+				$result = $sqlexec ->fetchAll();
+				foreach ($result as $row ) {
+					echo("<div class='book'><img src='".$row['img']."' class='book_cover'><br><h4 class='book_title'>".$row['title']."</h4><br></div>");
+
+				}
+
+				?>
+		    </div>
         </div>
-
-		
-		<div class="user-container">
-		      <button type = "submit" name = "submitConnection">
-			  <i class="fa fa-user icon"></i>
-			  Se connecter
-			  </button>
-         </div>
-      </nav>
-	      <?php
-		      if( file_exists("page_".$page.".inc.php") ){ 
-		        include("page_".$page.".inc.php");
-	      }
-	    	?>
-
-         <div id="caroussel">
-         	<?php 
-	         	$sql ="select * from book Limit 10";
-	         	$sqlexec = $db->prepare($sql);
-	         	$sqlexec ->execute();
-	         	$result = $sqlexec ->fetchAll();
-	         	foreach ($result as $row ) {
-	         		echo("<div class='book'><img src='".$row['img']."' class='book_cover'><br><h4 class='book_title'>".$row['title']."</h4><br></div>");
-
-	         	}
-
-         		?>
-         </div>
-      </div>
    </body>
 
 
